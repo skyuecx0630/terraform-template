85a9764956ec89c82e063761d45d5b5726e4ae0c
hmoon630 <skyuecx0630@gmail.com>
2024-06-11 13:40:05 +0900

    add comments to identify blue/green params

--- codepipeline/variables.tf
+++ codepipeline/variables.tf
@@ -19,12 +19,14 @@ variable "pipelines" {
       codebuild_name                = "myapp-build"
       codebuild_ecr_repository_name = "myapp"
 
+      codedeploy_ecs_cluster_name = "skills-cluster"
+      codedeploy_ecs_service_name = "myapp"
+
+      # Only for Blue/Green
       codedeploy_create                = true
       codedeploy_application_name      = "myapp-deploy"
       codedeploy_deployment_group_name = "myapp-dg"
 
-      codedeploy_ecs_cluster_name        = "skills-cluster"
-      codedeploy_ecs_service_name        = "myapp"
       codedeploy_ecs_listener_arn        = "arn:aws:elasticloadbalancing:us-east-1:856210586235:listener/app/myapp-alb/18ac23c1e9907228/5cc19c4b2434ce82"
       codedeploy_ecs_target_group_1_name = "myapp-tg"
       codedeploy_ecs_target_group_2_name = "myapp-tg-2"
